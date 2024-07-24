a81fa213f4f5d34a9504b97495e79e93b0384810
cachecrab <aniseed.truffle.0g@icloud.com>
2024-07-24 01:47:39 +0100

    remove commented temp code

--- src/root.zig
+++ src/root.zig
@@ -1,33 +1,5 @@
 // Micrograd in Zig
 
-// export fn godbolt() f64 {
-//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-//     defer _ = gpa.deinit();
-
-//     const allocator = gpa.allocator();
-
-//     var buf = ValueBuf.init(allocator) catch unreachable;
-//     defer buf.deinit();
-
-//     const x1 = buf.leaf(2) catch unreachable;
-//     const x2 = buf.leaf(0) catch unreachable;
-//     const w1 = buf.leaf(-3) catch unreachable;
-//     const w2 = buf.leaf(1) catch unreachable;
-//     const b = buf.leaf(6.8813735870195432) catch unreachable;
-//     const x1w1 = buf.mul(x1, w1) catch unreachable;
-//     const x2w2 = buf.mul(x2, w2) catch unreachable;
-//     const x1w1x2w2 = buf.add(x1w1, x2w2) catch unreachable;
-//     const n = buf.add(x1w1x2w2, b) catch unreachable;
-//     const o = buf.tanh(n) catch unreachable;
-
-//     var order = buf.build_topological_order(allocator, o) catch unreachable;
-//     defer order.deinit(allocator);
-
-//     buf.backward(order.items);
-
-//     return buf.get_grad(x1).*;
-// }
-
 const std = @import("std");
 const testing = std.testing;
 
@@ -79,10 +51,6 @@ const ValRefSet = struct {
 
     fn contains(self: Self, ref: ValRef) bool {
         return self.inner.contains(ref);
-    }
-
-    fn iterator(self: Self) Set.KeyIterator {
-        return self.inner.keyIterator();
     }
 
     fn deinit(self: *Self, allocator: Allocator) void {
